e2fc2b979dc8af0ca31b9296fab22e68c999a632
Phạm Quốc Khánh <22520650@gm.uit.edu.vn>
2025-11-06 18:59:09 +0700

    setup convex db

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
+"use client";
 
+import { Button } from "@workspace/ui/components/button";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Widget</h1>
         <Button size="sm">Button</Button>
-        <Input />
+        {JSON.stringify(users)}
       </div>
     </div>
   );
