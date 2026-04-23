88c0c5b67a8a3981c19b5d2797e43aacae121b76
Phạm Quốc Khánh <22520650@gm.uit.edu.vn>
2026-04-23 19:43:42 +0700

    add conversation lay

---
+++ apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
@@ -0,0 +1,23 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@workspace/ui/components/resizable";
+
+export const ConversationsLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <ResizablePanelGroup className="h-full flex-1" direction="horizontal">
+      <ResizablePanel defaultSize={30} maxSize={30} minSize={20}>
+        <p>Conversations</p>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel className="h-full" defaultSize={70}>
+        {children}
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
