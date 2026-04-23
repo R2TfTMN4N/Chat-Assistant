76b5ba5947078b3fa62c927f6ddfde0b0b93b0f1
Phạm Quốc Khánh <22520650@gm.uit.edu.vn>
2026-04-23 19:43:45 +0700

    add search tool

--- packages/backend/convex/public/messages.ts
+++ packages/backend/convex/public/messages.ts
@@ -7,6 +7,7 @@ import { supportAgent } from "../system/ai/agents/supportAgent";
 import { escalateConversation } from "../system/ai/tools/escalateConversation";
 import { resolveConversation } from "../system/ai/tools/resolveConversation";
 import { saveMessage } from "@convex-dev/agent";
+import { search } from "../system/ai/tools/search";
 
 export const create = action({
   args: {
@@ -55,8 +56,9 @@ export const create = action({
         {
           prompt: args.prompt,
           tools: {
-            escalateConversation,
-            resolveConversation,
+            escalateConversationTool: escalateConversation,
+            resolveConversationTool: resolveConversation,
+            searchTool: search,
           },
         }
       );
--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -4,8 +4,9 @@ import { components } from "../../../_generated/api";
 import { google } from "@ai-sdk/google";
 import { resolveConversation } from "../tools/resolveConversation";
 import { escalateConversation } from "../tools/escalateConversation";
+import { SUPPORT_AGENT_PROMPT } from "../constants";
 
 export const supportAgent = new Agent(components.agent, {
   chat: openai.chat("gpt-4o-mini"),
-  instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation and "escalateConversation" tool when user requests to escalate the conversation to a human operator or expresses frustration.`,
+  instructions: SUPPORT_AGENT_PROMPT,
 });
