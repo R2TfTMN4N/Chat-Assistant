9d37228e96e1ba961e712196eb081cb476b0edaf
Phạm Quốc Khánh <22520650@gm.uit.edu.vn>
2026-04-23 19:43:38 +0700

    add error wg screen

--- apps/widget/modules/widget/atoms/widget-atoms.ts
+++ apps/widget/modules/widget/atoms/widget-atoms.ts
@@ -1,4 +1,8 @@
 import { atom } from "jotai";
 import { WidgetScreen } from "../types";
 
-export const screenAtom = atom<WidgetScreen>("auth");
+export const screenAtom = atom<WidgetScreen>("error");
+
+export const errorMessageAtom = atom<string | null>(
+  "Invalid configuration. Please contact support."
+);
