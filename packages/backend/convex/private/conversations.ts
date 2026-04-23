d3877670eed4cd477067e9a1cb4cf4e7ee6a343b
Phạm Quốc Khánh <22520650@gm.uit.edu.vn>
2026-04-23 19:43:43 +0700

    add update status api

--- packages/backend/convex/private/conversations.ts
+++ packages/backend/convex/private/conversations.ts
@@ -1,9 +1,50 @@
 import { ConvexError, v } from "convex/values";
-import { query } from "../_generated/server";
+import { mutation, query } from "../_generated/server";
 import { supportAgent } from "../system/ai/agents/supportAgent";
 import { MessageDoc } from "@convex-dev/agent";
 import { paginationOptsValidator, PaginationResult } from "convex/server";
 import { Doc } from "../_generated/dataModel";
+
+export const updateStatus = mutation({
+  args: {
+    conversationId: v.id("conversations"),
+    status: v.union(
+      v.literal("unresolved"),
+      v.literal("resolved"),
+      v.literal("escalated")
+    ),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+    const orgId = identity.orgId as string;
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+    const conversation = await ctx.db.get(args.conversationId);
+    if (!conversation) {
+      throw new ConvexError({
+        code: "not_found",
+        message: "Conversation not found",
+      });
+    }
+    if (conversation.organizationId !== orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "You do not have access to this conversation",
+      });
+    }
+    await ctx.db.patch(args.conversationId, { status: args.status });
+  },
+});
 
 export const getOne = query({
   args: {
